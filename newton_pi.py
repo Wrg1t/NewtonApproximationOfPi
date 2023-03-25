3578c4ac254fd8e4eb9828972481249fc81d1c76
Wrg1t <wr0n9@outlook.com>
2023-03-25 15:38:58 +0800

    rename variables, improve comments and codes.

--- newton_pi.py
+++ newton_pi.py
@@ -1,48 +1,60 @@
 from decimal import Decimal, getcontext
-from os import system
+import os
 
-# set the precision of decimal arithmetic.
-precision = 5000
-getcontext().prec = precision
+# Set the precision of decimal arithmetic.
+PRECISION = 10000
+getcontext().prec = PRECISION
 
-# set the size of the cmd window.
-system("mode con cols=200 lines=30")
+# Set the size of the cmd window.
+os.system("mode con cols=200 lines=55")
 
-print(f'Current precision: {precision} digit(s)')
+print(f'Current precision: {PRECISION} digit(s)')
 print('More terms would have a more accurate result.')
 
-terms = int(input("Input the terms: "))
+num_terms = int(input("Input the number of terms to use in the approximation: "))
 
-n = Decimal('1') / Decimal('2')
-content = Decimal('0')
-tri_area = Decimal('3') ** Decimal('0.5') / Decimal('8')
-arrg = Decimal('2') / Decimal('3')
-fct = Decimal('1')
+exponent = Decimal('1') / Decimal('2')
+approximation = Decimal('0')  # Initial approximation.
+triangle_area = Decimal('3') ** Decimal('0.5') / Decimal('8')  # Area of a 30-60-90 triangle in the unit circle.
+binomial_coeff = Decimal('2') / Decimal('3')  # Initial value for binomial coefficient.
+factorial = Decimal('1')  # Initial value for factorial.
+prev_pi = Decimal('0')
 
-for term in range(0, terms):
+for term_index in range(num_terms):
+    # Calculate the next term in the approximation using the binomial theorem.
+    binomial_coeff *= exponent - term_index + 1 
 
-    # n*(n-1)*(n-2)...
-    arrg = arrg * (n - term + 1)
-    result = arrg
+    # Update the factorial for the term.
+    if term_index != 0:
+        factorial *= term_index
 
-    # factorial
-    if term != 0:
-        fct *= term
+    # Determine the sign of the term.
+    if term_index % 2 == 0:
+        term_sign = 1
+    else:
+        term_sign = -1
 
-    result /= fct
+    # Calculate the denominator of the term, which is an odd number.
+    term_denominator = 2 * term_index + 1
 
-    if result == 1:
-        result = result
-    elif result > 0:
-        result = -result
+    # Calculate the final value of the term and add it to the approximation.
+    term_value = term_sign * binomial_coeff / (factorial * term_denominator * 2 ** term_denominator)
+    approximation += term_value
 
-    odd = 2 * (term + 1) - 1
+    # Print the current approximation to pi.
+    pi = Decimal('12') * (approximation - triangle_area)
 
-    result /= odd
-    result /= 2**odd
+    if pi == prev_pi:
+        print('\nReached same value of pi as previous iteration. Terminating loop.')
+        break
+    
+    # Clear the terminal, move the cursor to the first row and first column, and print the latest value of pi.
+    print("\033[2J", end="", flush=True)
+    print("\033[1;1H", end="", flush=True)
+    print(f'\r{term_index}  {pi}', end='', flush=True)
+    
+    prev_pi = pi
 
-    content += result
 
-    print(term, ' ', Decimal('12') * (content - tri_area))
-
-system('pause')
+# Pause the program before exiting.
+input('Press Enter to exit...')
